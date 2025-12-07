a51122f2d4dc5574c858574f73dbc25825775286
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-12-07 18:45:19 +0330

    responsive [errorFill,errorLength]

--- pages/components/errorLength.js
+++ pages/components/errorLength.js
@@ -4,7 +4,7 @@ function ErrorLength({setErrorLength}) {
     return (
         <>
             <div className='absolute right-10 top-10'>
-                <div className='bg-red-400 w-[16vw] h-[8vh] rounded-2xl flex items-center gap-5 justify-center' >
+                <div className='bg-red-400 w-[200px] sm:w-[300px]  h-[8vh] rounded-2xl flex items-center gap-5 justify-center' >
                     <button className=' size-10 rounded-full  bg-white cursor-pointer' onClick={()=>setErrorLength(false)}>❌ </button>
                     <h1>Your Password is too short</h1>
                 </div>
