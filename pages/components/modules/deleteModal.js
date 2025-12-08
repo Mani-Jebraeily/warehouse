e98b242f71d23ae5e1a11e88ab4812c18458b517
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-12-08 13:52:28 +0330

    responsive [Modal Delete]

--- pages/components/modules/deleteModal.js
+++ pages/components/modules/deleteModal.js
@@ -41,10 +41,10 @@ function DeleteModal({ setShowDelete, refresh, setRefresh, selectedId }) {
   return (
     <>
       <div className="fixed inset-0 bg-black/50 flex items-center justify-center">
-        <div className='flex flex-col justify-center items-center gap-10 w-[472px] h-[338px] rounded-4xl bg-[#FFFFFF]'>
+        <div className='flex flex-col justify-center items-center gap-10 w-[80vw] sm:w-[472px] h-[338px] rounded-4xl bg-[#FFFFFF]'>
           <Image src={Icon} alt='Icon delete' />
           <h2 className='font-normal text-[20px] text-[#282828] '>Delete This product?</h2>
-          <div className='flex gap-8'>
+          <div className=' flex gap-2 sm:gap-8 flex-col-reverse sm:flex-row'>
             <button onClick={() => setShowDelete(false)} className='w-40 h-10 bg-[#DFDFDF] p-2.5 rounded-xl font-semibold text-lg text-[#282828CC] cursor-pointer flex justify-center items-center'>Cancel</button>
             <button onClick={deleteHandeler} className='w-40 h-10 bg-[#F43F5E] p-2.5 rounded-xl font-semibold text-lg  text-[#FFFFFF] cursor-pointer flex justify-center items-center'>Delete</button>
           </div>
