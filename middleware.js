7297c552b063000121eadf178e211fcb8fe93c18
Mani-Jebraeily <mani.jebraeily1382@gmail.com>
2025-12-10 20:39:35 +0330

    change  redirect 404 Page

--- middleware.js
+++ middleware.js
@@ -4,12 +4,10 @@ export function middleware(req) {
   const token = req.cookies.get('token')
   const { pathname } = req.nextUrl
 
-  // ❌ یوزر لاگین نیست، ولی رفته صفحه اصلی
   if (!token && pathname === '/') {
     return NextResponse.redirect(new URL('/login', req.url))
   }
 
-  // ✅ یوزر لاگینه، ولی رفته صفحات guest
   if (token && (pathname === '/login' || pathname === '/register')) {
     return NextResponse.redirect(new URL('/', req.url))
   }
--- pages/404.js
+++ pages/404.js
@@ -1,16 +1,17 @@
 import { redirect } from 'next/dist/server/api-utils'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
 
 function NotFound() {
+    const router = useRouter()
+  
+  useEffect(()=>{
+      router.push("/login")
+  },[])
   return (
-    <div>NotFound</div>
+    <div className=' w-screen h-screen flex justify-center items-center text-3xl'>NotFound Page</div>
   )
 }
 
 export default NotFound
-
-// export async function getStaticProps(){
-//     return{
-//         redirect:{destination:"/login"}
-//     }
-// }
